f7a6743fe7a68f00f7620d2e1995562394d0f605
M <mchaker@users.noreply.github.com>
2021-09-24 04:30:16 -0700

    Replace `which` with `command -v`
    `which` has been deprecated in Debian

--- lib/utils.sh
+++ lib/utils.sh
@@ -77,7 +77,7 @@
 #
 # Use colors, but only if connected to a terminal, and that terminal
 # supports them.
-if which tput >/dev/null 2>&1; then
+if command -v tput >/dev/null 2>&1; then
     ncolors=$(tput colors)
 fi
 if [ -t 1 ] && [ -n "$ncolors" ] && [ "$ncolors" -ge 8 ]; then
